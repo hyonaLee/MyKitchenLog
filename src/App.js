ad3d304d638ffeb85e7e56c326f859c9be3a1781
hyunha <fly9739@gmail.com>
2021-10-12 13:05:58 +0900

    2021.10.12 Main GET catch 오류 수정

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import './App.css';
-import Header from './Components/View/Header/Header';
-import Main from './Components/View/Main/Main';
-import Write from './Components/View/Main/Write';
-import Detail from './Components/View/Main/Detail';
-import {Route} from 'react-router-dom';
-
-
+import "./App.css";
+import Header from "./Components/View/Header/Header";
+import Main from "./Components/View/Main/Main";
+import Write from "./Components/View/Main/Write";
+import Detail from "./Components/View/Main/Detail";
+import { Route } from "react-router-dom";
+import React, { useState } from "react";
 
 function App() {
+  
+  //detail에서 write로 클린된 id값 보내기.
+  const [clickid, setClickid] = useState(0);
+    console.log("ID값", clickid);
+  function getData(data) {
+    setClickid(data);
+  }
+
   return (
     <div>
-        <Header/>
-        <Route exact path="/" component={Main}/>
-        <Route exact path="/Write" component={Write}/>
-        <Route exact path="/Detail" component={Detail}/>
-        <Route exact path="/Detail/:id" component={Detail}/>
+      <Header />
+      <Route exact path="/" component={Main} />
+      <Route exact path="/Write">
+        <Write clickid={clickid}/>
+      </Route>
+      <Route exact path="/Detail/Write/:id">
+        <Write clickid={clickid} />
+      </Route>
+      <Route exact path="/Detail/:id">
+        <Detail getData={getData} />
+      </Route>
     </div>
   );
 }
--- src/Components/View/Main/Main.js
+++ src/Components/View/Main/Main.js
@@ -1,66 +1,66 @@
-import styled from 'styled-components';
-import React, { useState , useEffect } from 'react';
-import axios from "axios"
-import { Link } from 'react-router-dom';
-import '../../../App.css';
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import "../../../App.css";
 
 const Main = () => {
   const [blogData, setBlogData] = useState([]);
 
-    // server로부터 data 요청
-    useEffect(() => {
-      axios
-          .get("http://localhost:3001/posts")
-          .then(response => {
-          //   console.log("받은데이터", response);
-          // console.log("받은데이터", response.data);
-          setBlogData(response.data);
-          })
-          .catch(console.log("에러 데이터를 못받음"));
-    }, [setBlogData]);
-    // console.log(typeof(blogData))
-    // console.log(blogData)
+  // server로부터 data 요청
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/posts")
+      .then((response) => {
+        //   console.log("받은데이터", response);
+        // console.log("받은데이터", response.data);
+        setBlogData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [setBlogData]);
+  // console.log(typeof(blogData))
+  // console.log(blogData)
 
-    return (
-        <MainDiv>
+  return (
+    <MainDiv>
       {blogData.map((blogData) => {
         return (
           <Link to={`Detail/${blogData.id}`}>
-          <div className="list" key={blogData.id}>
-            <h2 className="listfont">My Kitchen 레시피 - {blogData.title}</h2>
-          </div>
-      </Link>
-        )
+            <div className="list" key={blogData.id}>
+              <h2 className="listfont">My Kitchen 레시피 - {blogData.title}</h2>
+            </div>
+          </Link>
+        );
       })}
-        </MainDiv>
-
-    );
-}
+    </MainDiv>
+  );
+};
 
 const MainDiv = styled.div`
-display: grid;
-grid-template-columns: repeat(5, 1fr);
-grid-gap: 50px 0px;
-overflow: hidden;
-position: absolute;
-padding: 50px 0px;
-top: 80px;
-width: 100%;
-background-color: ivory;
-color: black;
-@media screen and (max-width: 1600px) {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 50px 0px;
+  overflow: hidden;
+  position: absolute;
+  padding: 50px 0px;
+  top: 80px;
+  width: 100%;
+  background-color: ivory;
+  color: black;
+  @media screen and (max-width: 1600px) {
     grid-template-columns: repeat(4, 1fr);
-   }
-@media screen and (max-width: 1200px) {
+  }
+  @media screen and (max-width: 1200px) {
     grid-template-columns: repeat(3, 1fr);
-   }
-@media screen and (max-width: 870px) {
+  }
+  @media screen and (max-width: 870px) {
     grid-template-columns: repeat(2, 1fr);
-}
-@media screen and (max-width: 550px) {
+  }
+  @media screen and (max-width: 550px) {
     grid-template-columns: repeat(1, 1fr);
-}
+  }
 `;
-
 
 export default Main;
